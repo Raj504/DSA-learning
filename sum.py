a3210c631a458fac0b1ca4d331be89760fe53f9f
Raj504 <rajaryan8930@gmail.com>
2026-08-17 12:29:48 +0530

    added sum.py

---
+++ sum.py
@@ -0,0 +1,8 @@
+def add_two_number(a, b):
+    add = a + b
+    return add
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+result = add_two_number(number1, number2)
+print("The sum is:", result)
