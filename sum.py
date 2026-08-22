72b9decfdda805d4c3122f0c3ad6db2b3ba0d466
Raj504 <rajaryan8930@gmail.com>
2026-08-23 01:04:16 +0530

    Add arithmetic functions for subtraction, multiplication, and division

--- sum.py
+++ sum.py
@@ -2,7 +2,25 @@ def add_two_number(a, b):
     add = a + b
     return add
 
+def subtract_two_number(a, b):
+    subtract = a - b
+    return subtract
+
+def multiply_two_numbers(a,b):
+    multiply = a * b
+    return multiply
+
+def divide_two_numbers(a, b):
+    divide = a / b
+    return divide
+
 number1 = int(input("Enter first number: "))
 number2 = int(input("Enter second number: "))
-result = add_two_number(number1, number2)
-print("The sum is:", result)
+result1 = add_two_number(number1, number2)
+result2 = subtract_two_number(number1, number2)
+result3 = multiply_two_numbers(number1, number2)
+result4 = divide_two_numbers(number1, number2)
+print("The sum is:", result1)
+print("The difference is:", result2)
+print("The product is:", result3)
+print("The quotient is:", result4)
